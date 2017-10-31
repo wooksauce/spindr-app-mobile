9213c0f917ddcadb9fbe4501acdc3d80eee0c9d8
Alex Ko <alexkosj@gmail.com>
2017-10-30 17:27:25 -0700

    confirm that client receives msg from backend emit

--- mobile/components/Chat.js
+++ mobile/components/Chat.js
@@ -12,46 +12,45 @@ class Chat extends Component {
     this.state = {
       messages: []
     }
-    this.socket = SocketIOClient('http://localhost:3000');
-    this.socket.on('message', (message) => {
-      let oldMessages = this.state.messages;
-      // React will automatically rerender the component when a new message is added.
-      this.setState({ messages: oldMessages.concat(message) });
-    });
+  
     this.onSend = this.onSend.bind(this);
-    this.determineUser = this.determineUser.bind(this);
+    this.fetchChat = this.fetchChat.bind(this);
+    this.appendMsg = this.appendMsg.bind(this);
   }
 
   componentDidMount() {
-    console.log('IDDDDD:', this.props.navigation.state.params.userId);
+    this.socket = SocketIOClient('http://localhost:3000');
+    this.socket.on('message', this.appendMsg);
+      // let oldMessages = this.state.messages;
+      // // React will automatically rerender the component when a new message is added.
+      // this.setState({ messages: oldMessages.concat(message) });
+  }
+
+  appendMsg(message) {
+    console.log('MESSAGE FROM SERVER:', message);
+    // let oldMessages = this.state.messages;
+    // console.log('old msgs:', oldMessages);
+    // // React will automatically rerender the component when a new message is added.
+    // this.setState((previousState) => ({
+    //   messages: [message],
+    // }));
+ 
+  }
+
+  fetchChat() {
+
   }
 
   onSend(messages = []) {
-    this.setState((previousState) => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }));
-    axios.post('http://localhost:3000/api/chats', {
-      user_one: this.props.navigation.state.params.userId,
-      chat_entry: messages[0].text
-    })
+    // this.setState((previousState) => ({
+    //   messages: GiftedChat.append(previousState.messages, messages),
+    // }));
+    // axios.post('http://localhost:3000/api/chats', {
+    //   user_one: this.props.navigation.state.params.userId,
+    //   chat_entry: messages[0].text
+    // })
     this.socket.emit('message', messages[0]);
     console.log('Message sent:', messages[0]);
-  }
-
-  determineUser() {
-    AsyncStorage.getItem('userId')
-      .then((userId) => {
-        if (userId) {
-          this.socket.emit('i-dont-need-an-id');
-        } else {
-          this.socket.emit('i-need-id');
-          this.socket.on('here-is-your-id', (id) => {
-            AsyncStorage.setItem('userId', id);
-            // Force a rerender in the React component
-            this.setState({ id });
-          });
-        }
-    });
   }
 
   render() {
