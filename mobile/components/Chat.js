18d0d6b32c8f06258211fd885bc55f80118b7f78
Alex Ko <alexkosj@gmail.com>
2017-10-30 20:30:59 -0700

    render chat messages upon submission

--- mobile/components/Chat.js
+++ mobile/components/Chat.js
@@ -4,53 +4,31 @@ import { GiftedChat } from 'react-native-gifted-chat';
 import SocketIOClient from 'socket.io-client';
 import axios from 'axios';
 
-const USER_ID = '@userId';
-
 class Chat extends Component {
   constructor(props) {
     super(props);
     this.state = {
       messages: []
     }
-  
     this.onSend = this.onSend.bind(this);
-    this.fetchChat = this.fetchChat.bind(this);
-    this.appendMsg = this.appendMsg.bind(this);
   }
 
   componentDidMount() {
     this.socket = SocketIOClient('http://localhost:3000');
-    this.socket.on('message', this.appendMsg);
-      // let oldMessages = this.state.messages;
-      // // React will automatically rerender the component when a new message is added.
-      // this.setState({ messages: oldMessages.concat(message) });
-  }
-
-  appendMsg(message) {
-    console.log('MESSAGE FROM SERVER:', message);
-    // let oldMessages = this.state.messages;
-    // console.log('old msgs:', oldMessages);
-    // // React will automatically rerender the component when a new message is added.
-    // this.setState((previousState) => ({
-    //   messages: [message],
-    // }));
- 
-  }
-
-  fetchChat() {
-
+    this.socket.on('message', this.onSend);
+    this.socket.emit('joinRoom', 'test room');
   }
 
   onSend(messages = []) {
-    // this.setState((previousState) => ({
-    //   messages: GiftedChat.append(previousState.messages, messages),
-    // }));
+    console.log('MESSAGES:', messages);
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
     // axios.post('http://localhost:3000/api/chats', {
     //   user_one: this.props.navigation.state.params.userId,
     //   chat_entry: messages[0].text
     // })
-    this.socket.emit('message', messages[0]);
-    console.log('Message sent:', messages[0]);
+    this.socket.emit('message', messages[0], 'test room');
   }
 
   render() {
@@ -58,9 +36,6 @@ class Chat extends Component {
       <GiftedChat
         messages={this.state.messages}
         onSend={(messages) => this.onSend(messages)}
-        user={{
-          _id: 1,
-        }}
       />
     );
   }
