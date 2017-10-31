676fb1b350e7f75a375c7b76bf0d5894ac37e606
Alex Ko <alexkosj@gmail.com>
2017-10-30 21:13:21 -0700

    move chat nav into matches component

--- mobile/components/Matches.js
+++ mobile/components/Matches.js
@@ -3,11 +3,16 @@ import { View, Text, StyleSheet, Button, StatusBar } from 'react-native';
 
 class Matches extends Component {
   render() {
+    const { navigate } = this.props.navigation;
     return (
       <View>
         <StatusBar
         barStyle='light-content'/>
         <Text>Matches are gonna be shown here</Text>
+        <Text>Say hi to Tara for me Mark</Text>
+        <Button
+        title = 'Chats'
+        onPress = {() => navigate('Chat')} />
       </View>
     );
   }
